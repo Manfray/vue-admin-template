115cd755280ea2d0ea06d36c8caa2a5a6c6926fc
Manfray <960940021@qq.com>
2021-12-21 10:37:22 +0800

    initial

---
+++ src/views/Demand/index.vue
@@ -0,0 +1,123 @@
+<template>
+  <div class="app-container">
+    <el-form :model="formInline" class="demo-form-inline" label-width="120px">
+      <el-row :gutter="10">
+        <el-col :md="8">
+          <el-form-item label="需求名称名称">
+            <el-input v-model="formInline.user" placeholder="请输入需求名称" />
+          </el-form-item>
+        </el-col>
+        <el-col :md="8">
+          <el-form-item label="需求状态">
+            <el-select v-model="formInline.region" placeholder="请输入需求状态">
+              <el-option label="区域一" value="shanghai" />
+              <el-option label="区域二" value="beijing" />
+            </el-select>
+          </el-form-item>
+        </el-col>
+        <el-col :md="8">
+          <el-button type="primary" @click="onSubmit">查询</el-button>
+          <el-button @click="onSubmit">重置</el-button>
+        </el-col>
+      </el-row>
+
+    </el-form>
+    <el-table
+      v-loading="listLoading"
+      :data="list"
+      element-loading-text="Loading"
+      fit
+      highlight-current-row
+    >
+      <el-table-column align="center" label="ID" width="95">
+        <template slot-scope="scope">
+          {{ scope.$index }}
+        </template>
+      </el-table-column>
+      <el-table-column label="Title">
+        <template slot-scope="scope">
+          {{ scope.row.title }}
+        </template>
+      </el-table-column>
+      <el-table-column label="Author" width="110" align="center">
+        <template slot-scope="scope">
+          <span>{{ scope.row.author }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="Pageviews" width="110" align="center">
+        <template slot-scope="scope">
+          {{ scope.row.pageviews }}
+        </template>
+      </el-table-column>
+      <el-table-column class-name="status-col" label="Status" width="110" align="center">
+        <template slot-scope="scope">
+          <el-tag :type="scope.row.status | statusFilter">{{ scope.row.status }}</el-tag>
+        </template>
+      </el-table-column>
+      <el-table-column align="center" prop="created_at" label="Display_time" width="200">
+        <template slot-scope="scope">
+          <i class="el-icon-time" />
+          <span>{{ scope.row.display_time }}</span>
+        </template>
+      </el-table-column>
+    </el-table>
+    <!-- 分页 -->
+    <el-pagination
+      :current-page="1"
+      :page-sizes="[100, 200, 300, 400]"
+      :page-size="100"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="400"
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+    />
+  </div>
+</template>
+
+<script>
+import { getList } from '@/api/table'
+
+export default {
+  filters: {
+    statusFilter(status) {
+      const statusMap = {
+        published: 'success',
+        draft: 'gray',
+        deleted: 'danger'
+      }
+      return statusMap[status]
+    }
+  },
+  data() {
+    return {
+      list: null,
+      listLoading: true,
+      formInline: {
+        user: '',
+        region: ''
+      }
+    }
+  },
+  created() {
+    this.fetchData()
+  },
+  methods: {
+    fetchData() {
+      this.listLoading = true
+      getList().then(response => {
+        this.list = response.data.items
+        this.listLoading = false
+      })
+    },
+    handleSizeChange(val) {
+      console.log(`每页 ${val} 条`)
+    },
+    handleCurrentChange(val) {
+      console.log(`当前页: ${val}`)
+    },
+    onSubmit() {
+      console.log('submit!')
+    }
+  }
+}
+</script>
---
+++ src/views/dashboard/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="dashboard-container">
+    <!-- <div class="dashboard-text">name: {{ name }}</div> -->
+    <div class="dashboard-text">引导页</div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+console.log('加载dashboard组件')
+export default {
+  name: 'Dashboard',
+  computed: {
+    ...mapGetters([
+      'name'
+    ])
+  },
+  created() {
+    console.log('dashboard created')
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.dashboard {
+  &-container {
+    margin: 30px;
+  }
+  &-text {
+    font-size: 30px;
+    line-height: 46px;
+  }
+}
+</style>
